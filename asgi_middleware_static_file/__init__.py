ee3c7b629fba8720b4cf73d269dcc2c35e4666be
Rex Zhang <rex.zhang@gmail.com>
2022-09-07 14:30:36 +0800

    Update, pep517/pep621

--- asgi_middleware_static_file/__init__.py
+++ asgi_middleware_static_file/__init__.py
@@ -2,20 +2,6 @@
 # coding=utf-8
 
 
-"""
-ASGI Middleware Static File
-"""
+from .core import ASGIMiddlewarePath, ASGIMiddlewareStaticFile  # noqa: F401
 
-from .core import (  # noqa: F401
-    ASGIMiddlewareStaticFile,
-    ASGIMiddlewarePath,
-)
-
-__version__ = "0.4.0"
-
-__author__ = "Rex Zhang"
-__author_email__ = "rex.zhang@gmail.com"
-__licence__ = "MIT"
-
-__description__ = "ASGI Middleware for serving Static File."
-__project_url__ = "https://github.com/rexzhang/asgi-middleware-static-file"
+VERSION = "0.4.0"
